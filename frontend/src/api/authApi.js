912b8aa454acdb7e8a416064189a859012c49936
Joshikrunal <146454902+Joshikrunal@users.noreply.github.com>
2026-05-27 11:46:10 -0400

    Delete frontend directory

--- frontend/src/api/authApi.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const API = axios.create({ baseURL: 'http://localhost:5001/api' })
-
-export const login = (formData) => API.post('/auth/login', formData)
-export const register = (formData) => API.post('/auth/register', formData)
-
-export default API
