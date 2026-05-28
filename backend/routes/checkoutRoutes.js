57a669eb4566d4b0753cd841287432bbbbe36b0e
Joshikrunal <146454902+Joshikrunal@users.noreply.github.com>
2026-05-28 11:46:36 -0400

    Delete backend directory

--- backend/routes/checkoutRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createCheckoutSession } = require('../controllers/stripeController');
-// Assuming you have an auth middleware to protect this route
-// const { protect } = require('../middleware/authMiddleware'); 
-
-router.post('/create-session', createCheckoutSession); // Add 'protect' here if users must be logged in
-
-module.exports = router;
--- backend/routes/customRequestRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createCustomRequest } = require('../controllers/customRequestController');
-
-router.post('/', createCustomRequest);
-
-module.exports = router;
